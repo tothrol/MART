32e043ab551fb8269bb8f667a4eb6f7011ecb1ad
Stefko <60323626+StefanFlaschko@users.noreply.github.com>
2022-11-11 19:47:30 +0100

    f

--- src/stores/statsStore.js
+++ src/stores/statsStore.js
@@ -18,11 +18,37 @@ export const useStatsStore = defineStore('statsStore', {
       // queryUsageStats('test');
       // queryUsageStats('tt');
       // let stats = await echo.echo({ value: 'test' });
-      let stats = await echo.echo();
-      console.log('STATS STOREEEEEEEEP', stats);
-      console.log(stats.value);
-      console.log('STATS 3', stats);
-      // echo.stats('statTest');
+
+      let permission = await echo.checkForUsageStatsPermission();
+      console.log('STATSPermission', permission);
+      let stats = await echo.getStats();
+
+      // let statsString = JSON.stringify(stats).replace(/\\/g, '');
+      // let statsString2 = statsString.replace;
+      // console.log('STATS 1: ' + stats);
+      // console.log('STATS 2: ' + stats.androidUsageStats);
+
+      // let myObject = JSON.parse(stats.androidUsageStats);
+      // console.log('STATS 6: ' + myObject);
+
+      let newString = stats.androidUsageStats.slice(
+        2,
+        stats.androidUsageStats.length - 2
+      );
+      // console.log('STATS 1: ', newString);
+
+      let statsArray = newString.split('},{');
+      console.log('STATS 2: ', statsArray);
+      // console.log('STATS 3: ', statsArray[1]);
+
+      // let statsString = newString.replace(/({})/g, '');
+      // console.log('STATS 8: ' + statsString);
+
+      // var newJson = stats.replace(/([a-zA-Z0-9]+?):/g, '"$1":');
+      // newJson = newJson.replace(/'/g, '"');
+
+      // var data = JSON.parse(newJson);
+      // console.log('STATS 7: ' + data);
     },
   },
   getters: {},
